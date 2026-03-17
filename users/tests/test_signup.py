1679bd5bac1ed7a6b23670954aa966542b16fb79
sdfz24 <sdfirmenich@uloyola.es>
2026-03-17 22:02:23 +0100

    test for fail

--- users/tests/test_signup.py
+++ users/tests/test_signup.py
@@ -11,7 +11,7 @@ class SignupEndpointTests(APITestCase):
     def test_signup_create_successfully(self):
         payload = {
             "username": "newuser",
-            "first_name": "NewUserName",
+            "first_name": "NewUserName2",
             "last_name": "User",
             "email": "newuser@example.com",
         }
